61bcf9ace7ff8a169fcba4939daa10cec09ba472
Matthew Jiang <matthewyjiang@gmail.com>
2025-06-05 19:26:11 -0700

    update nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,10 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
 -- Display line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -14,7 +21,7 @@ vim.opt.shiftwidth = 4       -- Indentation width
 vim.opt.softtabstop = 4      -- Number of spaces a tab counts for while editing
 vim.opt.smartindent = true   -- Smarter auto-indenting
 
+vim.lsp.enable('clangd')
+vim.lsp.enable('pyright')
 
 require("config.lazy")
-
-
